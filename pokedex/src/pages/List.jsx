79debe93957dd2d035fae0d55887306b066d3379
doray <d.balassoupramanien@ecole-ipssi.net>
2022-01-14 17:38:14 +0100

    1 st generation list pokemon done, bug after reload for title with helmet

--- pokedex/src/pages/List.jsx
+++ pokedex/src/pages/List.jsx
@@ -1,20 +1,31 @@
 import { Helmet } from "react-helmet";
 import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-const products = [
-  {
-    id: 1,
-    name: "Bulbizarre",
-    href: "#",
-    imageSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: "$35",
-    color: "Black",
-  },
-];
+const imgPoke =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
-function List() {
+const noImgPoke =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png";
+
+const List = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await axios.get(
+          "https://pokeapi.co/api/v2/pokemon?limit=151"
+        );
+        setData(data.data.results);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <Helmet>
@@ -30,18 +41,18 @@ function List() {
         <div className="bg-white">
           <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
             <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-              {products.map((product) => (
-                <div key={product.id} className="group relative">
+              {data.map((data, index) => (
+                <div key={index} className="group relative">
                   <h3 className="text-sm text-gray-700">
-                    <a href={product.href}>
+                    <a href={data.href}>
                       <span aria-hidden="true" className="absolute inset-0" />
-                      {product.name}
+                      {data.name}
                     </a>
                   </h3>
                   <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
                     <img
-                      src={product.imageSrc}
-                      alt={product.imageAlt}
+                      src={imgPoke + (index + 1) + ".png"}
+                      alt={noImgPoke}
                       className="w-full h-full object-center object-cover lg:w-full lg:h-full"
                     />
                   </div>
@@ -53,6 +64,6 @@ function List() {
       </main>
     </>
   );
-}
+};
 
 export default List;
